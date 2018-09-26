c6e21302a2d098965222f5e402db7a26fea41824
Lola Onanuga <slonanuga@gmail.com>
2018-09-26 11:03:10 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map {|x| x.push(!)}
+  planeteer_calls.map {|x| x.push('!')}
 end
 
 def long_planeteer_calls# code an argument here
