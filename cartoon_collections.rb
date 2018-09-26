211dbcfc2bfe6621f577410a4372ed3e1237c2de
Lola Onanuga <slonanuga@gmail.com>
2018-09-26 11:20:40 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,6 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(cheese)
- cheese.find {|x|  x == "cheddar"
+ cheese.find {|x|  x == "cheddar" || x == "gouda" || x == "camembert"}
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
